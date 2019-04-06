103176de40196c3fc51cc7e9aac5b0533bc2d79b
Alan Bonham <abo37@sbgml04502.sbg.skybettingandgaming.com>
2019-04-06 16:29:11 +0100

    [PHASE-002]

--- game.js
+++ game.js
@@ -16,7 +16,7 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('leo', 'assets/sprites/Leo.png');
+    this.load.image('leo', 'assets/sprites/Leobig.png');
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('red', 'assets/particles/red.png');
 }
@@ -37,6 +37,18 @@ function create() {
     leo.setVelocity(100, 200);
     leo.setBounce(1, 1);
     leo.setCollideWorldBounds(true);
-
     emitter.startFollow(leo);
 }
+
+
+// var sprite = game.add.sprite(game.world.randomX, game.world.randomY, 'disk');
+
+//         //  Pick a random number between -2 and 6
+//         var rand = game.rnd.realInRange(-2, 6);
+
+//         //  Set the scale of the sprite to the random value
+//         sprite.scale.setTo(rand, rand);
+
+//         //  You can also scale sprites like this:
+//         //  sprite.scale.x = value;
+//         //  sprite.scale.y = value;
